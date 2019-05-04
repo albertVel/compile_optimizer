09ce9eb2d1e7dc7bb7d075988fb3a58101807891
Albert Velasco <albert.velasco@roche.com>
2019-05-04 21:00:00 +0200

    First commit

---
+++ compile_optimization/compile_optimization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace compile_optimization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ compile_optimization/tests_compiler/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace tests_compiler
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
