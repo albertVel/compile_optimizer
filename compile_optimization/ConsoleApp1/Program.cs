8f2b4ab029def0753df38f72f87691b55b89a8da
Albert Velasco <albert.velasco@roche.com>
2019-05-05 20:23:01 +0200

    Revert "Remove ConsoleApp project"
    This reverts commit 1f3ac42376acbdeb185db6f58b6c27ecf4d30099.

---
+++ compile_optimization/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace compile_optimization
+{
+    class Program
+    {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
